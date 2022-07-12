8a0e04a326f1f6f0ff17a2d314b401d0f1a2825b
Camie0903 <camerontamboer03@gmail.com>
2022-07-12 16:27:32 +0200

    end of day

---
+++ src/views/Events.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="Events">
+    <div v-for="Event of Events" :key="Event.ID">
+    {{ Event.Title }}
+    <img :src="Event.img"/> 
+        </div>
+    </div>
+    <div v-else>Loading...</div>
+</template>
+<script>
+export default {
+    mounted() {
+        this.$store.dispatch("getEvents");
+    },
+    computed: {
+        Events(){
+          return  this.$store.state.Events;
+        },
+    },
+};
+</script>
+<style>
+    
+</style>
