d5bbdb13a0c56cfd38d8765b9fa8d0e241fe0111
hilmimallick <hilmi09mallick@gmail.com>
2022-07-12 12:15:54 +0200

    nav

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <p>20 Lower Long Street</p>
+    <p>Riversdale (Located on the Garden Route)</p>
+    <p>Western Cape</p>
+    <p>South Africa</p>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<style></style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="bpic">
+    <div class="container">
+      <div class="heading">
+        <h1><u>CASH EVENTS</u></h1>
+      </div>
+      <div class="text">
+        <p class="textp">
+          Visit Our Unique Family Owned Multipurpose Estate in Riversdale, the
+          first Garden Route Town when travelling from Cape Town. Classified as
+          an Estate with Four Stars by the Tourism Grading Council of South
+          Africa and located on a historical street in Riversdale.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+#bpic {
+  background-image: url("https://i.postimg.cc/4345LkQH/httpsi-postimg-cczv-F6-BBGVagriculture-5144904-1920-jpg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 98vh;
+  display: flex;
+  justify-content: center;
+}
+
+.heading {
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+}
+.text {
+  display: flex;
+  justify-content: center;
+  margin: 20px;
+}
+
+.textp {
+  font-size: 23px;
+}
+
+.container {
+  margin: auto;
+  padding: auto;
+  width: 800px;
+  height: 250px auto;
+  background-color: rgba(167, 165, 196, 0.3);
+  backdrop-filter: blur(3px);
+  border: 2px solid white;
+  border-radius: 10px;
+  background-clip: padding-box;
+  margin-bottom: 80px;
+}
+</style>
