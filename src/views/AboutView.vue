2362a943793ebab2ee9063279102d55ba13806cf
SmangeleMntungwa <mangie696@gmail.com>
2022-07-14 15:10:08 +0200

    About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,91 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <h1>Welcome to Our Estate</h1>
+    <br>
+    <h2> C-A-S-H Wine Estate was started by four Bestfriends/Collegues with slightly different likes, 
+      <br>that when put together became a perfect match.
+      <br>
+      <br>
+      We were very close to the point of sharing everything at work,
+      <br>
+       to forming a Lotto club where we combined our numbers and played Lotto.
+       <br>
+       Our goal was to always make money beause we believed money was destined for us, 
+       <br> as you can see the first letters of our names spell C-A-S-H. 
+ </h2> 
+    </div>
+
+
+     <div class="wrapper">
+          <div class="container  ">
+               <div class="banner-image"></div>
+               <img src="https://i.postimg.cc/fL3ZwwxV/Cameron-2.jpg" alt="" class="profile-image">
+               <h1 class="name">Cameron</h1>
+               <p class="description">I am a foodie, So I became the face of the Resturant</p>
+               <button class="btn">Follow</button>
+          </div>
+          <div class="container">
+               <div class="banner-image"></div>
+               <img src="https://i.postimg.cc/zf66FTgf/Ashwin-3.jpg" alt="" class="profile-image">
+               <h1 class="name">Ashwin</h1>
+               <p class="description"> I am the face of the Wine Production</p>
+               <button class="btn">Follow</button>
+          </div>
+          <div class="container">
+               <div class="banner-image"></div>
+               <img src="https://i.postimg.cc/qR1Dqnxw/Smangele-3-2.jpg" alt="" class="profile-image">
+               <h1 class="name">Smangele</h1>
+               <p class="description">I love Therapy so I became the face of the Spa</p>
+               <button class="btn">Follow</button>
+          </div>
+          <div class="container">
+               <div class="banner-image"></div>
+               <img src="https://i.postimg.cc/vBmF0GPP/Hilmi-2.jpg" alt="" class="profile-image">
+               <h1 class="name">Hilmi</h1>
+               <p class="description">I love golf, So I became the face of the Golf Club</p>
+               <button class="btn">Follow</button>
+          </div>
+     </div>
+
+
+<div class="offer">
+  <h1> WHAT WE OFFER?</h1>
+  <br>
+  Since we are a team of young people we love hosting events, we are even on the Top 3 of Best Events Hosting in South Arfrica. 
+  <br>
+ Bookings of dates to be day ONE YEAR ahead. 
+ <br>
+Below is a list of few Events we host. Please feel free to call and enquire incase what you want is not on the list.
+ <br>
+<h2>
+  <br>
+<ul>
+  <li>Weddings</li>
+  <li>Parties</li>
+  <li>Coporate Events</li>
+  <li>Wine Tasting</li>
+  <li>Food Events</li>
+  <li>Anniversaries</li>
+   <li>Team Building</li>
+  <li>Concerts</li>
+  <li>Mini races</li>
+   <li>Golf Tournaments</li>
+  <li>Spa and Therapy</li>
+</ul>  
+
+ </h2>
+</div>
+
+
 </template>
+
+ <style scoped>
+ 
+ .about{
+  
+ }
+
+ 
+
+
+ </style>
